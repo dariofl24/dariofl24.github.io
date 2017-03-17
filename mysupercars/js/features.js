c56c9d156c407f612f3e328563f1c9751959987e
Dario Flores <dario.fl24@gmail.com>
2017-03-16 23:33:47 -0600

    Initial page commits part 2

---
+++ mysupercars/js/features.js
@@ -0,0 +1,38 @@
+
+var mySlickInit = {
+
+    init: function() {
+        console.log("mySlickInit !!!!");
+        $("section.regular").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        fade: true,
+        arrows: true,
+        autoplaySpeed: 4000,
+        cssEase: 'linear'
+      });
+
+        $("section.featureshow").slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        fade: true,
+        arrows: false,
+        autoplaySpeed: 3000,
+        cssEase: 'linear'
+      });
+
+    },
+
+    bindEvents: function(){
+
+    }
+
+};
+ 
+$(document).on('ready',mySlickInit.init);
