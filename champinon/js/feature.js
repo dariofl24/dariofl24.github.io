c8bb2435f4b17b10666d9da2153137733f0f35d9
Dario Flores <dflores@tacitknowledge.com>
2019-01-04 10:41:45 -0600

    Add features js initial

---
+++ champinon/js/feature.js
@@ -0,0 +1,34 @@
+
+var allFeatures = (function() { 
+
+	var person_info_url = "https://torre.bio/api/people/sergiomg77";
+
+    var init = function(){
+
+        initPersonInfo();
+
+    };
+
+    var initPersonInfo = function(){
+
+    	$.get(person_info_url, function(data, status){
+
+    		if(status == 'success'){
+    			console.log(data);
+    		}else{
+    			console.log("No data !");
+    		}
+
+    	});
+
+    }
+
+    return {
+
+        allinit: init
+
+    };
+
+})();
+
+$(document).on('ready',allFeatures.allinit);
