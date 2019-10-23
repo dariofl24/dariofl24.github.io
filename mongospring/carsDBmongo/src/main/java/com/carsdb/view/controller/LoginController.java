c4899680cc9cfb9fdc55afcb4cd50fdd48429f26
Dario Flores <dflores@tacitknowledge.com>
2019-10-23 10:12:48 -0500

    Adds Spring security prototype

---
+++ mongospring/carsDBmongo/src/main/java/com/carsdb/view/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.carsdb.view.controller;
+
+import com.carsdb.view.abs.AbstractViewController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController extends AbstractViewController
+{
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String getLogin()
+    {
+        return "login";
+    }
+}
